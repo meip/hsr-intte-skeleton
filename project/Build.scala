48d6894bbed1fa1526613310f485770e00eab73c
Philipp Meier <p1meier@hsr.ch>
2013-11-05 20:29:56 +0100

    Refactor: custom Build.scala

---
+++ project/Build.scala
@@ -0,0 +1,59 @@
+import sbt._
+import Keys._
+
+object BuildSettings {
+  val buildVersion = "0.0.1"
+
+  val buildSettings = Defaults.defaultSettings ++ Seq(
+    organization := "ch.hsr.intte.uebungXX",
+    version := buildVersion,
+    shellPrompt := ShellPrompt.buildShellPrompt,
+    exportJars := true
+  )
+}
+
+object Resolvers {
+  val sunrepo = "Sun Maven2 Repo" at "http://download.java.net/maven/2"
+  val sunrepoGF = "Sun GF Maven2 Repo" at "http://download.java.net/maven/glassfish"
+  val oraclerepo = "Oracle Maven2 Repo" at "http://download.oracle.com/maven"
+
+  val oracleResolvers = Seq(sunrepo, sunrepoGF, oraclerepo)
+}
+
+object Dependencies {
+  val jettywebapp = "org.eclipse.jetty" % "jetty-webapp" % "8.1.7.v20120910" % "container,test"
+  val jettyjsp = "org.eclipse.jetty" % "jetty-jsp" % "8.1.7.v20120910" % "container,test"
+  val javaxservlet = "org.eclipse.jetty.orbit" % "javax.servlet" % "3.0.0.v201112011016" % "container,test" artifacts Artifact("javax.servlet", "jar", "jar")
+  val javaxservletjsp = "org.eclipse.jetty.orbit" % "javax.servlet.jsp" % "2.2.0.v201112011158" % "container,test" artifacts Artifact("javax.servlet.jsp", "jar", "jar")
+  val logbackclassic = "ch.qos.logback" % "logback-classic" % "1.0.6"
+}
+
+object IntteUBuild extends Build {
+
+  import Dependencies._
+  import BuildSettings._
+  import com.earldouglas.xsbtwebplugin.WebPlugin.webSettings
+
+  // Sub-project inttewebapp specific dependencies
+  val intteWebappDeps = Seq(
+    jettywebapp,
+    jettyjsp,
+    javaxservlet,
+    javaxservletjsp,
+    logbackclassic
+  )
+
+  lazy val intteu = Project(
+    "intteu",
+    file("."),
+    settings = buildSettings ++ Seq(
+      name := "hsr-intte-skeleton"
+    )
+  ) dependsOn (inttewebapp)
+
+  lazy val inttewebapp = Project(
+    "intteu-webapp",
+    file("webapp"),
+    settings = buildSettings ++ webSettings ++ Seq(libraryDependencies ++= intteWebappDeps)
+  )
+}
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,6 +1,7 @@
-addSbtPlugin("com.earldouglas" % "xsbt-web-plugin" % "0.3.0")
-
 //Enable the sbt idea plugin
+addSbtPlugin("com.earldouglas" % "xsbt-web-plugin" % "0.4.2")
+
+//Enable the sbt eclipse plugin
 addSbtPlugin("com.github.mpeltonen" % "sbt-idea" % "1.5.1")
 
 //Enable the sbt eclipse plugin
